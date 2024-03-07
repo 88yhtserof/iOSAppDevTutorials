3066973db29e2fa6fcaad3f42dd35ae99a5edd47
88yhtserof <88yhtserofg@gmail.com>
2024-03-07 15:06:40 +0900

    Create an enumeration for rows

---
+++ Today/Today/DetailViewController/ReminderViewController+Row.swift
@@ -0,0 +1,42 @@
+//
+//  ReminderViewController+Row.swift
+//  Today
+//
+//  Created by 임윤휘 on 3/7/24.
+//
+
+import UIKit
+
+extension ReminderViewController {
+    // diffable data source는 snapshot 간 어떤 element가 변경되었는지 결정하기 위해 hash 값을 사용한다.
+    // 이를 위해 열거형을 hashable하게 만든다.
+    // 열거형을 추가하는 것은 각 행을 간단 명료하게 이미지화하며, 이는 view controller가 각 행에 적절한 텍스트를 제공하도록 돕는다.
+    enum Row: Hashable {
+        case date
+        case notes
+        case time
+        case title
+        
+        var imageName: String? {
+            switch self {
+            case .date: return "calender.circle"
+            case .notes: return "square.and.pencil"
+            case .time: return "clock"
+            default: return nil
+            }
+        }
+        
+        var image: UIImage? {
+            guard let imageName = imageName else { return nil }
+            let configuration = UIImage.SymbolConfiguration(textStyle: .headline)
+            return UIImage(systemName: imageName, withConfiguration: configuration)
+        }
+        
+        var textStyle: UIFont.TextStyle {
+            switch self {
+            case .title: return .headline
+            default: return .subheadline
+            }
+        }
+    }
+}
--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -22,4 +22,15 @@ class ReminderViewController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
     }
+    
+    // 주어진 행과 관련한 텍스트를 반환하는 함수
+    // if문 대신 열거형을 사용하여 각 case를 구분한다면, 각 행 수정이 쉬워지고 이후 reminder의 상세를 더 추가할 수 있어 확장적이다
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title: return reminder.title
+        }
+    }
 }
