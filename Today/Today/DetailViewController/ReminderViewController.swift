f8f41363c4aab9cd4129a74e45a11d014522dbd0
88yhtserof <88yhtserofg@gmail.com>
2024-03-07 17:05:52 +0900

    Set up the data source

--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
+    // DataSource는 제너릭 타입이다.
+    // 제너릭 파라미터로 Int와 Row를 지정했으므로 DataSource가 section number로 Int를, list의 row로 Row 타입을 사용함을 컴파일러에게 알려줄 수 있다.
+    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    
     var reminder: Reminder
+    private var dataSource: DataSource!
     
     init(reminder: Reminder) {
         self.reminder = reminder
@@ -21,6 +26,25 @@ class ReminderViewController: UICollectionViewController {
     // code-based로 reminder view controller를 생성했기 때문에 아래 이니셜라이저는 사용되지 않는다
     required init?(coder: NSCoder) {
         fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+    
+    override func viewDidLoad() {
+        // view controller의 life cycle을 오버라이드할 때, 사용자 지정 작업을 수행하기 전에 부모 클래스의 작업이 우선 시행되어야 한다.
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView) { (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+    /// cell과 indexPath row를 받아 collectionview에 cell 등록
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguaration = cell.defaultContentConfiguration()
+        contentConfiguaration.text = text(for: row)
+        contentConfiguaration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguaration.image = row.image
+        cell.contentConfiguration = contentConfiguaration
+        cell.tintColor = .todayPrimaryTint
     }
     
     // 주어진 행과 관련한 텍스트를 반환하는 함수
