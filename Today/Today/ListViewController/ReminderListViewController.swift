8d2d7c939f95fafbcf5940dd9be3eabbb8b99924
88yhtserof <88yhtserofg@gmail.com>
2024-05-02 11:43:35 +0900

    Filtering reminders: Display a segmented control

--- Today/Today/ListViewController/ReminderListViewController.swift
+++ Today/Today/ListViewController/ReminderListViewController.swift
@@ -19,6 +19,7 @@ class ReminderListViewController: UICollectionViewController {
             $0.dueDate < $1.dueDate
         }
     }
+    let listStyleSegmentedControl = UISegmentedControl(items: [ReminderListStyle.today.name, ReminderListStyle.future.name, ReminderListStyle.all.name])
 
     // viewController가 자신의 view 계층을 메모리에 로드한 후 시스템은 ViewDiddLoad를 호출한다.
     override func viewDidLoad() {
@@ -39,6 +40,10 @@ class ReminderListViewController: UICollectionViewController {
         let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didPressAddButton(_:)))
         addButton.accessibilityLabel = NSLocalizedString("Add reminder", comment: "Add button accessibility label")
         navigationItem.rightBarButtonItem = addButton
+        
+        listStyleSegmentedControl.selectedSegmentIndex = listStyle.rawValue
+        navigationItem.titleView = listStyleSegmentedControl
+        
         if #available(iOS 16, *) {
             navigationItem.style = .navigator
         }
